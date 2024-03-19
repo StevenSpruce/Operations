59d9ff14159fd495df269835e4804610ad18a21f
DominicStone-DfE <157606275+DominicStone-DfE@users.noreply.github.com>
2024-03-19 12:08:29 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -584,7 +584,7 @@ router.post('/SBRDCalcMethod', function (req, res) {
   var calcmethod = req.session.data['calcmethod']
 
   // Check whether the variable matches a condition
-  if (calcmethod == "SBC01"||calcmethod == "SBC03"||calcmethod == "HGV01"){
+  if (calcmethod == "SBC01"||calcmethod == "SBC03"||calcmethod == "HGV01"||calcmethod == "HGV02"){
     // Send user to next page
     res.redirect('/SBRD_Start')
   } else {
