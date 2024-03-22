046d19d2e04546a684cf4181c1bad4b007b5f05f
DominicStone-DfE <157606275+DominicStone-DfE@users.noreply.github.com>
2024-03-22 18:12:59 +0000

    payyment

--- app/routes.js
+++ app/routes.js
@@ -584,7 +584,7 @@ router.post('/SBRDCalcMethod', function (req, res) {
   // Check whether the variable matches a condition
   if (calcmethod == "SBC01"||calcmethod == "SBC03"||calcmethod == "HGV01"||calcmethod == "HGV02"||calcmethod == "HGV03"||calcmethod == "HGV04"||calcmethod == "HGV05"){
     // Send user to next page
-    res.redirect('/SBRD_ContractDatesCost')
+    res.redirect('/SBRD_PaymentDetails')
   } else {
     // Send user to ineligible page
     res.redirect('/SBRD_MidCourse')
@@ -599,7 +599,7 @@ router.post('/SBRDDetails', function (req, res) {
   // Check whether the variable matches a condition
   if (calcmethod == "SBC01"||calcmethod == "SBC03"){
     // Send user to next page
-    res.redirect('/SBRD_Qualifying')
+    res.redirect('/SBRD_PaymentDetails')
   } if (calcmethod == "HGV01"){
     // Send user to next page
     res.redirect('/SBRD_Start')
